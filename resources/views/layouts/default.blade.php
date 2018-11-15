308fed8a36ce3a6f8b91dc1966db459716945368
vagrant <vagrant@homestead>
2018-11-15 08:54:09 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,6 +11,7 @@
             @include('shared._messages')
 	        @yield('content')
 	        @include('layouts._footer')
-	    </div>    
+	    </div> 
+        <script src="/js/app.js"></script>   
   	</body>
 </html>
